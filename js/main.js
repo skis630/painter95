f43595d603048eb12d416e988ee81fb88a6fd7da
Sara <skis630@aucklanduni.ac.nz>
2019-06-12 08:43:32 +0300

    New features added

--- js/main.js
+++ js/main.js
@@ -1,39 +1,67 @@
 var color = "black";
+var penSize = "3px";
+
+//features
 $(".colors .well").click(function() {
     color = $(this).css("background-color");
 });
 
+$(".dropdown-menu li div").click(function() {
+    penSize = $(this).css("height");
+});
+$("#custom-color").change(function(e) {
+    color = $(this).val();
+});
+  
+//paint
 $("#canvas-wrapper").click(function(e) {
     var rect = document.getElementById("canvas-wrapper").getBoundingClientRect();
+
     var circle = $("<span />").css("background-color", color).css("border-radius", "50%").
-                               css("display", "inline-block").css("width", "3px").css("height", "3px").
+                               css("display", "inline-block").css("width",penSize).css("height", penSize).
                                css("position", "absolute").css("top", `${e.offsetY}px`).
-                               css("left", `${e.offsetX}px`);
-    if (parseInt(e.offsetX) < 5) {
-        circle.css("left", "18px");
-    }
+                               css("left", `${e.offsetX + 15}px`);
     $(this).append(circle);
+
+    var posX = (e.offsetX + 15) / rect.width;
+    var posY = e.offsetY / rect.height;
+
+    if (posX > 0.95 && posY > 0.95) {
+        $(this).off("click");
+    }
+
+
 });
 
 $("#canvas-wrapper").mousedown(function(event) {
+    var rect = document.getElementById("canvas-wrapper").getBoundingClientRect();
     $(this).mousemove(function(e) {
         var line = $("<span />").css("background-color", color).css("display", "inline-block").
-        css("width", "3px").css("height", "3px").
+        css("width", penSize).css("height", penSize).
         css("position", "absolute").css("top", `${e.offsetY}px`).
-        css("left", `${e.offsetX}px`);
-    if (parseInt(e.offsetX) < 5) {
-        line.css("left", "20px");
-        }
+        css("left", `${e.offsetX + 15}px`).css("border-radius", "50%");
+   
     $(this).append(line);
     $(this).on("mouseup", function() {
         $(this).off("mousemove");
-    })
     });
-   
-})
 
-$("#custom-color").change(function(e) {
-    color = $(this).val();
+    var posX = (e.offsetX + 15) / rect.width;
+    var posY = e.offsetY / rect.height;
+
+    if (posX > 0.95 && posY > 0.95) {
+        $(this).off("mousemove");
+    }    
+  })
 })
 
 //$(".colors > div").clicl(function() )
+$(".clr").click(function() {
+    $("#canvas-wrapper").html("");
+})
+
+
+var left = $("#canvas-wrapper span").css("left");
+$("#canvas-wrapper span").filter(function() {
+        return $(this).css("left").substring(0, left.length - 2) < 100;
+      }).remove();
